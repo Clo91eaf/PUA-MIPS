8870a74de92eccefa7136430a22da23901ead46d
Liphen <1076187250@qq.com>
2023-03-18 12:12:32 +0800

    style：修改gitignore、makefile；增加一个test模块

---
+++ chisel/src/test/scala/test.scala
@@ -0,0 +1,12 @@
+// ANDtest.scala
+package cpu.puamips
+
+import chisel3.stage.ChiselGeneratorAnnotation
+
+object testMain extends App {
+  (new chisel3.stage.ChiselStage).execute(
+    Array("--target-dir", "generated/get"),
+    Seq(ChiselGeneratorAnnotation(() => new Decoder))
+  )
+}
+
