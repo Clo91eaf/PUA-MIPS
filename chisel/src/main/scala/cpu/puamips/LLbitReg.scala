dada3ea563bd04aaeb86ac2712bd5f56c98971de
Liphen <1076187250@qq.com>
2023-03-29 14:08:57 +0800

    feat<LLbitReg>: 模块增加

---
+++ chisel/src/main/scala/cpu/puamips/LLbitReg.scala
@@ -0,0 +1,28 @@
+package cpu.puamips
+
+import Const._
+import chisel3._
+
+class LLbitReg extends Module {
+  val io = IO(new Bundle {
+    val fromWriteBackStage = Flipped(new WriteBackStage_LLbitReg())
+
+    val memory = new LLbitReg_Memory()
+    val flush = Input(Bool())
+  })
+  // input
+
+  // output
+  val LLbit = RegInit(false.B)
+  io.memory.LLbit := LLbit
+
+  when(io.flush) {
+    LLbit := false.B
+  }.elsewhen(io.fromWriteBackStage.LLbit_we === WRITE_ENABLE) {
+    LLbit := io.fromWriteBackStage.LLbit_value
+  }
+
+  // debug
+  printf(p"LLbitReg :LLbit 0x${Hexadecimal(LLbit)}\n")
+
+}
