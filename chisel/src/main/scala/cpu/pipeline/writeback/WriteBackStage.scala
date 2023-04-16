10bb297f4888234c7291d034eb18cf2bf88d342f
Liphen <1076187250@qq.com>
2023-04-16 09:19:27 +0800

    feat<wbStage>: 支持例外

--- chisel/src/main/scala/cpu/pipeline/writeback/WriteBackStage.scala
+++ chisel/src/main/scala/cpu/pipeline/writeback/WriteBackStage.scala
@@ -9,6 +9,7 @@ class WriteBackStage extends Module {
   val io = IO(new Bundle {
     val fromMemory = Flipped(new Memory_WriteBackStage())
     val fromCP0    = Flipped(new CP0_WriteBackStage())
+    val ext_int    = Input(UInt(6.W))
 
     val llbitReg   = new WriteBackStage_LLbitReg()
     val memory     = new WriteBackStage_Memory()
@@ -22,74 +23,101 @@ class WriteBackStage extends Module {
     val decoder    = new WriteBackStage_Decoder()
   })
   // input
+  val ws_pc              = RegInit(BUS_INIT)
+  val ws_reg_waddr       = RegInit(ADDR_BUS_INIT)
+  val ws_reg_wen         = RegInit(REG_WRITE_DISABLE)
+  val ws_reg_wdata       = RegInit(BUS_INIT)
+  val ws_hi              = RegInit(BUS_INIT)
+  val ws_lo              = RegInit(BUS_INIT)
+  val ws_whilo           = RegInit(WRITE_DISABLE)
+  val ws_LLbit_wen       = RegInit(false.B)
+  val ws_LLbit_value     = RegInit(false.B)
+  val ws_valid           = RegInit(false.B)
+  val ws_inst_is_mtc0    = RegInit(false.B)
+  val ws_inst_is_mfc0    = RegInit(false.B)
+  val ws_inst_is_eret    = RegInit(false.B)
+  val ws_inst_is_syscall = RegInit(false.B)
+  val ws_bd              = RegInit(false.B)
+  val ws_badvaddr        = RegInit(false.B)
+  val ws_cp0_addr        = RegInit(0.U(8.W))
+  val ws_excode          = RegInit(0.U(5.W))
+  val ws_ex              = RegInit(false.B)
+  val ext_int            = io.ext_int
+  // input-cp0
+  val cp0_rdata = io.fromCP0.cp0_rdata
 
   // output
-  val pc           = RegInit(BUS_INIT)
-  val reg_waddr    = RegInit(ADDR_BUS_INIT)
-  val reg_wen      = RegInit(REG_WRITE_DISABLE)
-  val reg_wdata    = RegInit(BUS_INIT)
-  val hi           = RegInit(BUS_INIT)
-  val lo           = RegInit(BUS_INIT)
-  val whilo        = RegInit(WRITE_DISABLE)
-  val LLbit_wen    = RegInit(false.B)
-  val LLbit_value  = RegInit(false.B)
-  val cp0_wen      = RegInit(WRITE_DISABLE)
-  val cp0_waddr    = RegInit(CP0_ADDR_BUS_INIT)
-  val cp0_wdata    = RegInit(BUS_INIT)
-  val inst_is_mfc0 = RegInit(false.B)
   val allowin      = Wire(Bool())
-  val ws_valid     = RegInit(false.B)
-  val eret         = false.B
-  val ex           = false.B
+  val eret         = Wire(Bool())
+  val ex           = Wire(Bool())
+  val inst_is_mfc0 = Wire(Bool())
+  val cp0_we       = Wire(Bool())
+  val cp0_wdata    = Wire(UInt(32.W))
+  val cp0_epc      = Wire(UInt(32.W))
+  val cp0_status   = Wire(UInt(32.W))
+  val cp0_cause    = Wire(UInt(32.W))
 
   // output-reg file
-  io.regFile.reg_waddr := reg_waddr
-  io.regFile.reg_wen   := reg_wen & Fill(4, ws_valid)
-  io.regFile.reg_wdata := reg_wdata
+  io.regFile.reg_waddr := ws_reg_waddr
+  io.regFile.reg_wen   := ws_reg_wen & Fill(4, ws_valid)
+  io.regFile.reg_wdata := Mux(ws_inst_is_mfc0, cp0_rdata, ws_reg_wdata)
 
   // output-hilo
-  io.hilo.hi    := hi
-  io.hilo.lo    := lo
-  io.hilo.whilo := whilo
+  io.hilo.hi    := ws_hi
+  io.hilo.lo    := ws_lo
+  io.hilo.whilo := ws_whilo
 
   // output-fetchStage
-  io.fetchStage.eret := eret
-  io.fetchStage.ex   := ex
+  io.fetchStage.eret    := eret
+  io.fetchStage.ex      := ex
+  io.fetchStage.cp0_epc := cp0_epc
 
   // output-decoder
   io.decoder.eret         := eret
   io.decoder.ex           := ex
   io.decoder.inst_is_mfc0 := inst_is_mfc0
-  io.decoder.reg_waddr    := reg_waddr
+  io.decoder.reg_waddr    := ws_reg_waddr
 
   // output-execute
-  io.execute.hi    := hi
-  io.execute.lo    := lo
-  io.execute.whilo := whilo
+  io.execute.hi    := ws_hi
+  io.execute.lo    := ws_lo
+  io.execute.whilo := ws_whilo
   io.execute.eret  := eret
   io.execute.ex    := ex
 
   // output-mov
-  io.mov.cp0_wen   := cp0_wen
-  io.mov.cp0_waddr := cp0_waddr
+  io.mov.cp0_wen   := cp0_we
+  io.mov.cp0_waddr := ws_cp0_addr
   io.mov.cp0_wdata := cp0_wdata
 
   // output-memory
-  io.memory.LLbit_wen   := LLbit_wen
-  io.memory.LLbit_value := LLbit_value
+  io.memory.LLbit_wen   := ws_LLbit_wen
+  io.memory.LLbit_value := ws_LLbit_value
   io.memory.allowin     := allowin
   io.memory.eret        := eret
   io.memory.ex          := ex
 
   // output-llbit reg
-  io.llbitReg.LLbit_wen   := LLbit_wen
-  io.llbitReg.LLbit_value := LLbit_value
+  io.llbitReg.LLbit_wen   := ws_LLbit_wen
+  io.llbitReg.LLbit_value := ws_LLbit_value
 
   // output-debug
-  io.debug.pc    := pc
+  io.debug.pc    := ws_pc
   io.debug.waddr := io.regFile.reg_waddr
   io.debug.wen   := Fill(4, io.regFile.reg_wen.orR)
   io.debug.wdata := io.regFile.reg_wdata
+
+  // output-cp0
+  io.cp0.wb_ex       := ws_ex
+  io.cp0.wb_bd       := ws_bd
+  io.cp0.eret_flush  := ws_inst_is_eret
+  io.cp0.wb_excode   := ws_excode
+  io.cp0.wb_pc       := ws_pc
+  io.cp0.wb_badvaddr := ws_badvaddr
+  io.cp0.ext_int_in  := ext_int
+  io.cp0.cp0_addr    := ws_cp0_addr
+  io.cp0.mtc0_we     := cp0_we
+  io.cp0.cp0_wdata   := cp0_wdata
 
   // io-finish
 
@@ -102,17 +130,37 @@ class WriteBackStage extends Module {
 
   when(io.fromMemory.valid && allowin) {
     // input-memory
-    reg_waddr    := io.fromMemory.reg_waddr
-    reg_wen      := io.fromMemory.reg_wen
-    reg_wdata    := io.fromMemory.reg_wdata
-    hi           := io.fromMemory.hi
-    lo           := io.fromMemory.lo
-    whilo        := io.fromMemory.whilo
-    LLbit_wen    := io.fromMemory.LLbit_wen
-    LLbit_value  := io.fromMemory.LLbit_value
-    pc           := io.fromMemory.pc
-    inst_is_mfc0 := io.fromMemory.inst_is_mfc0
+    ws_reg_waddr       := io.fromMemory.reg_waddr
+    ws_reg_wen         := io.fromMemory.reg_wen
+    ws_reg_wdata       := io.fromMemory.reg_wdata
+    ws_hi              := io.fromMemory.hi
+    ws_lo              := io.fromMemory.lo
+    ws_whilo           := io.fromMemory.whilo
+    ws_LLbit_wen       := io.fromMemory.LLbit_wen
+    ws_LLbit_value     := io.fromMemory.LLbit_value
+    ws_pc              := io.fromMemory.pc
+    ws_inst_is_mfc0    := io.fromMemory.inst_is_mfc0
+    ws_inst_is_mtc0    := io.fromMemory.inst_is_mtc0
+    ws_inst_is_syscall := io.fromMemory.inst_is_syscall
+    ws_inst_is_eret    := io.fromMemory.inst_is_eret
+    ws_excode          := io.fromMemory.excode
+    ws_badvaddr        := io.fromMemory.badvaddr
+    ws_cp0_addr        := io.fromMemory.cp0_addr
+    ws_ex              := io.fromMemory.ex
+    ws_bd              := io.fromMemory.bd
+
   }
+
+  cp0_epc    := Fill(32, ws_valid) & io.fromCP0.cp0_epc
+  cp0_status := Fill(32, ws_valid) & io.fromCP0.cp0_status
+  cp0_cause  := Fill(32, ws_valid) & io.fromCP0.cp0_cause
+
+  eret         := ws_inst_is_eret && ws_valid
+  ex           := ws_ex && ws_valid
+  inst_is_mfc0 := ws_inst_is_mfc0 && ws_valid
+
+  cp0_we    := ws_inst_is_mtc0 && ws_valid && !ws_ex
+  cp0_wdata := ws_reg_wdata
 
   // debug
   // printf(p"writeBackStage :pc 0x${Hexadecimal(pc)}\n")
