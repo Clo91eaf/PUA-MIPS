545ca7107989544ce9afdc2ca9a48b971107ceeb
Clo91eaf <1303076137@qq.com>
2023-04-27 19:15:16 +0800

    feat<makefile>: 修改了一下makefile

---
+++ chisel/src/utils/tcl/show_example.tcl
@@ -0,0 +1,3 @@
+start_gui
+open_wave_database {F:\\NSCSCC\\sram_test\\lab10\\CPU_CDE_SRAM\\mycpu_sram_verify\\run_vivado\\mycpu_prj1\\mycpu.sim\\sim_1\\behav\\xsim\\tb_top_behav.wdb}
+open_wave_config {F:\\NSCSCC\\sram_test\\lab10\\CPU_CDE_SRAM\\mycpu_sram_verify\\run_vivado\\mycpu_prj1\\tb_top_behav.wcfg}
--- chisel/src/utils/tcl/show_wave.tcl
+++ chisel/src/utils/tcl/show_wave.tcl
@@ -1,3 +1,3 @@
 start_gui
 open_wave_database {F:\\NSCSCC\\PUA-MIPS\\vivado\\CPU_CDE_SRAM\\mycpu_sram_verify\\run_vivado\\mycpu_prj1\\mycpu.sim\\sim_1\\behav\\xsim\\tb_top_behav.wdb}
-open_wave_config {F:\\NSCSCC\\CPU_CDE_SRAM\\tb_top.wcfg}
+open_wave_config {F:\\NSCSCC\\PUA-MIPS\\vivado\\tb_top_behav.wcfg}
