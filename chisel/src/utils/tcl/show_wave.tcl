010d1a6b139ff3dc40b6c09c007d1b6c7bf6aba0
Clo91eaf <1303076137@qq.com>
2023-05-18 16:07:41 +0800

    refactor<Makefile>: 删除了多余的组件, 将项目迁移到linux上.

--- chisel/src/utils/tcl/show_wave.tcl
+++
@@ -1,4 +0,0 @@
-set test_top [lindex $argv 0]
-start_gui
-open_wave_database ${test_top}\\mycpu_axi_verify\\run_vivado\\PUA-MIPS\\mycpu.sim\\sim_1\\behav\\xsim\\tb_top_behav.wdb
-open_wave_config ${test_top}\\mycpu_axi_verify\\run_vivado\\PUA-MIPS\\PUA-MIPS.wcfg
