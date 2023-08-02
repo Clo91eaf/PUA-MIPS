8ccbe53e1c203f7c4ede837d55952856a89de38c
Liphen <xi.lifeng@qq.com>
2023-08-02 15:36:35 +0800

    feat: 接入乘除法IP核

--- chisel/src/main/scala/cpu/pipeline/execute/Div.scala
+++ chisel/src/main/scala/cpu/pipeline/execute/Div.scala
@@ -59,27 +59,42 @@ class Div extends Module {
 
   // 0为被除数，1为除数
   val unsignedDiv_sent = Seq.fill(2)(RegInit(false.B))
+  val unsignedDiv_done = RegInit(false.B)
   val signedDiv_sent   = Seq.fill(2)(RegInit(false.B))
+  val signedDiv_done   = RegInit(false.B)
 
   when(unsignedDiv.s_axis_dividend_tready && unsignedDiv.s_axis_dividend_tvalid) {
     unsignedDiv_sent(0) := true.B
+  }.elsewhen(io.ready && io.allow_to_go) {
+    unsignedDiv_sent(0) := false.B
   }
   when(unsignedDiv.s_axis_divisor_tready && unsignedDiv.s_axis_divisor_tvalid) {
     unsignedDiv_sent(1) := true.B
+  }.elsewhen(io.ready && io.allow_to_go) {
+    unsignedDiv_sent(1) := false.B
   }
 
   when(signedDiv.s_axis_dividend_tready && signedDiv.s_axis_dividend_tvalid) {
     signedDiv_sent(0) := true.B
+  }.elsewhen(io.ready && io.allow_to_go) {
+    signedDiv_sent(0) := false.B
   }
   when(signedDiv.s_axis_divisor_tready && signedDiv.s_axis_divisor_tvalid) {
     signedDiv_sent(1) := true.B
+  }.elsewhen(io.ready && io.allow_to_go) {
+    signedDiv_sent(1) := false.B
   }
 
-  when(io.allow_to_go) {
-    unsignedDiv_sent(0) := false.B
-    unsignedDiv_sent(1) := false.B
-    signedDiv_sent(0)   := false.B
-    signedDiv_sent(1)   := false.B
+  when(signedDiv.m_axis_dout_tvalid && !io.allow_to_go) {
+    signedDiv_done := true.B
+  }.elsewhen(io.allow_to_go) {
+    signedDiv_done := false.B
+  }
+
+  when(unsignedDiv.m_axis_dout_tvalid && !io.allow_to_go) {
+    unsignedDiv_done := true.B
+  }.elsewhen(io.allow_to_go) {
+    unsignedDiv_done := false.B
   }
   // 被除数和除数的valid信号
   signedDiv.s_axis_dividend_tvalid := io.start && !signedDiv_sent(0) && io.signed
@@ -95,7 +110,15 @@ class Div extends Module {
   unsignedDiv.s_axis_dividend_tdata := io.src1
   unsignedDiv.s_axis_divisor_tdata  := io.src2
 
-  io.ready  := Mux(io.signed, signedDiv.m_axis_dout_tvalid, unsignedDiv.m_axis_dout_tvalid)
-  io.result := Mux(io.signed, signedDiv.m_axis_dout_tdata, unsignedDiv.m_axis_dout_tdata)
+  io.ready := Mux(
+    io.signed,
+    signedDiv.m_axis_dout_tvalid || signedDiv_done,
+    unsignedDiv.m_axis_dout_tvalid || unsignedDiv_done,
+  )
+  val signedRes =
+    Cat(signedDiv.m_axis_dout_tdata(DATA_WID - 1, 0), signedDiv.m_axis_dout_tdata(HILO_WID - 1, DATA_WID))
+  val unsignedRes =
+    Cat(unsignedDiv.m_axis_dout_tdata(DATA_WID - 1, 0), unsignedDiv.m_axis_dout_tdata(HILO_WID - 1, DATA_WID))
+  io.result := Mux(io.signed, signedRes, unsignedRes)
 
 }
