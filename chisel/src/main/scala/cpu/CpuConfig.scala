e39ce739317c708443e5558b5ac3cd2bbc87cf61
Liphen <xi.lifeng@qq.com>
2023-08-03 19:50:28 +0800

    docs(cpuconfig): 增加注释

--- chisel/src/main/scala/cpu/CpuConfig.scala
+++ chisel/src/main/scala/cpu/CpuConfig.scala
@@ -1,15 +1,15 @@
 package cpu
 
 case class CpuConfig(
+    val build: Boolean = true,            // 是否为build模式
+    val hasCommitBuffer: Boolean = false, // 是否有提交缓存
     val decoderNum: Int = 2,              // 同时访问寄存器的指令数
     val commitNum: Int = 2,               // 同时提交的指令数
     val fuNum: Int = 2,                   // 功能单元数
     val hasMonitor: Boolean = false,      // 是否有监控模块
     val instFetchNum: Int = 4,            // iCache取到的指令数量
-    val build: Boolean = true,           // 是否为build模式
-    val hasCommitBuffer: Boolean = false, // 是否有提交缓存
-    val writeBufferDepth: Int = 16,
-    val mulClockNum: Int = 3,
-    val divClockNum: Int = 8,
-    val instBufferDepth: Int = 8,
+    val writeBufferDepth: Int = 16,       // 写缓存深度
+    val mulClockNum: Int = 3,             // 乘法器的时钟周期数
+    val divClockNum: Int = 8,             // 除法器的时钟周期数
+    val instBufferDepth: Int = 16,        // 指令缓存深度
 ) {}
