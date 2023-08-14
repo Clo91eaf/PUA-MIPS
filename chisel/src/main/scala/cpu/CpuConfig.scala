5af62fd4c3bdbb1ca81ccd1ebe82e5255717e93a
Liphen <xi.lifeng@qq.com>
2023-08-14 14:52:04 +0800

    fix(cache): 修改部分逻辑

--- chisel/src/main/scala/cpu/CpuConfig.scala
+++ chisel/src/main/scala/cpu/CpuConfig.scala
@@ -3,12 +3,12 @@ package cpu
 import chisel3.util._
 
 case class CpuConfig(
-    val build: Boolean = false,              // 是否为build模式
+    val build: Boolean = true,              // 是否为build模式
     val hasCommitBuffer: Boolean = false,    // 是否有提交缓存
     val decoderNum: Int = 2,                 // 同时访问寄存器的指令数
     val commitNum: Int = 2,                  // 同时提交的指令数
     val fuNum: Int = 2,                      // 功能单元数
-    val instFetchNum: Int = 4,               // iCache取到的指令数量
+    val instFetchNum: Int = 2,               // iCache取到的指令数量
     val instFifoDepth: Int = 8,              // 指令缓存深度
     val writeBufferDepth: Int = 16,          // 写缓存深度
     val mulClockNum: Int = 3,                // 乘法器的时钟周期数
@@ -22,11 +22,12 @@ case class BranchPredictorConfig(
 )
 
 case class CacheConfig(
-    nway: Int = 2,
-    nbank: Int = 8,
+    nway: Int = 2,  // 路数
+    nbank: Int = 8, // bank数
     nset: Int,
     bankWidth: Int, // bytes per bank
 ) {
+  val config          = CpuConfig()
   val indexWidth      = log2Ceil(nset)                   // 6
   val bankIndexWidth  = log2Ceil(nbank)                  // 3
   val bankOffsetWidth = log2Ceil(bankWidth)              // 3
@@ -35,7 +36,7 @@ case class CacheConfig(
   val tagvWidth       = tagWidth + 1                     // 21
   val bankWidthBits   = bankWidth * 8                    // 64
   val burstSize       = 16
-  val ninst           = 4                                // TODO:改成可随意修改的参数
+  val ninst           = config.instFetchNum              // TODO:改成可随意修改的参数
   require(isPow2(nset))
   require(isPow2(nway))
   require(isPow2(nbank))
--- chisel/src/main/scala/cpu/cache/ICache.scala
+++ chisel/src/main/scala/cpu/cache/ICache.scala
@@ -13,7 +13,7 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
   val nway: Int            = cacheConfig.nway
   val nset: Int            = cacheConfig.nset
   val nbank: Int           = cacheConfig.nbank
-  val ninst: Int           = cacheConfig.ninst
+  val ninst: Int           = cacheConfig.ninst // 取指令的数量
   val bankOffsetWidth: Int = cacheConfig.bankOffsetWidth
   val bankWidth: Int       = cacheConfig.bankWidth
   val tagWidth: Int        = cacheConfig.tagWidth
@@ -50,16 +50,17 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
   // | inst 0 | inst 1 | inst 2 | inst 3 |
   // |   32   |   32   |   32   |   32   |
   // =====================================
-  val valid = RegInit(VecInit(Seq.fill(nset * nbank)(VecInit(Seq.fill(ninst)(false.B)))))
+  val instperbank = bankWidth / 4 // 每个bank存储的指令数
+  val valid       = RegInit(VecInit(Seq.fill(nset * nbank)(VecInit(Seq.fill(instperbank)(false.B)))))
 
-  val data = Wire(Vec(nway, Vec(ninst, UInt(DATA_WID.W))))
+  val data = Wire(Vec(nway, Vec(instperbank, UInt(DATA_WID.W))))
   val tag  = RegInit(VecInit(Seq.fill(nway)(0.U(tagWidth.W))))
 
   // * should choose next addr * //
   val should_next_addr = (state === s_idle && !tlb_fill) || (state === s_save)
 
   val data_raddr = io.cpu.addr(should_next_addr)(indexWidth + offsetWidth - 1, bankOffsetWidth)
-  val data_wstrb = RegInit(VecInit(Seq.fill(nway)(VecInit(Seq.fill(ninst)(0.U(4.W))))))
+  val data_wstrb = RegInit(VecInit(Seq.fill(nway)(VecInit(Seq.fill(instperbank)(0.U(4.W))))))
 
   val tag_raddr = io.cpu.addr(should_next_addr)(indexWidth + offsetWidth - 1, offsetWidth)
   val tag_wstrb = RegInit(VecInit(Seq.fill(nway)(false.B)))
@@ -76,7 +77,7 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
   // * fence * //
   val fence_index = io.cpu.fence_addr(indexWidth + offsetWidth - 1, offsetWidth)
   when(io.cpu.fence && !io.cpu.icache_stall && !io.cpu.cpu_stall) {
-    valid(fence_index) := VecInit(Seq.fill(ninst)(false.B))
+    valid(fence_index) := VecInit(Seq.fill(instperbank)(false.B))
   }
 
   // * replace set * //
@@ -91,11 +92,13 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
   val cache_hit_available = cache_hit && io.cpu.tlb.translation_ok && !io.cpu.tlb.uncached
   val sel                 = tag_compare_valid(1)
 
-  val bank_offset = io.cpu.addr(0)(log2Ceil(ninst) + 1, 2)
-  val inst       = VecInit(Seq.tabulate(ninst)(i => Mux(i.U <= (3.U - bank_offset), data(sel)(i.U + bank_offset), 0.U)))
-  val inst_valid = VecInit(Seq.tabulate(ninst)(i => cache_hit_available && i.U <= (3.U - bank_offset)))
+  val bank_offset = io.cpu.addr(0)(log2Ceil(instperbank) + 1, 2)
+  val inst = VecInit(
+    Seq.tabulate(instperbank)(i => Mux(i.U <= (3.U - bank_offset), data(sel)(i.U + bank_offset), 0.U)),
+  )
+  val inst_valid = VecInit(Seq.tabulate(instperbank)(i => cache_hit_available && i.U <= (3.U - bank_offset)))
 
-  val saved = RegInit(VecInit(Seq.fill(ninst)(0.U.asTypeOf(new Bundle {
+  val saved = RegInit(VecInit(Seq.fill(instperbank)(0.U.asTypeOf(new Bundle {
     val inst  = UInt(PC_WID.W)
     val valid = Bool()
   }))))
@@ -103,15 +106,15 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
   val axi_cnt = Counter(cacheConfig.burstSize)
 
   // bank tag ram
-  for { i <- 0 until nway; j <- 0 until ninst } {
+  for { i <- 0 until nway; j <- 0 until instperbank } {
     val bank = Module(new SimpleDualPortRam(nset * nbank, DATA_WID, byteAddressable = true))
     bank.io.ren   := true.B
     bank.io.raddr := data_raddr
     data(i)(j)    := bank.io.rdata
 
     bank.io.wen   := data_wstrb(i)(j).orR
-    bank.io.waddr := Cat(rset, axi_cnt.value(log2Ceil(cacheConfig.burstSize) - 1, log2Ceil(ninst)))
-    bank.io.wdata := Mux(j.U === axi_cnt.value(log2Ceil(ninst) - 1, 0), io.axi.r.bits.data, 0.U)
+    bank.io.waddr := Cat(rset, axi_cnt.value(log2Ceil(cacheConfig.burstSize) - 1, log2Ceil(instperbank)))
+    bank.io.wdata := Mux(j.U === axi_cnt.value(log2Ceil(instperbank) - 1, 0), io.axi.r.bits.data, 0.U)
     bank.io.wstrb := data_wstrb(i)(j)
   }
 
@@ -171,7 +174,7 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
           arvalid := true.B
 
           rset := vset
-          (0 until ninst).foreach(i => data_wstrb(lru(vset))(i) := Mux(i.U === 0.U, 0xf.U, 0x0.U))
+          (0 until instperbank).foreach(i => data_wstrb(lru(vset))(i) := Mux(i.U === 0.U, 0xf.U, 0x0.U))
           tag_wstrb(lru(vset))   := true.B
           tag_wdata              := io.cpu.tlb.tag
           valid(vset)(lru(vset)) := true.B
@@ -180,8 +183,8 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
           lru(vset) := ~sel
           when(io.cpu.cpu_stall) {
             state := s_save
-            (1 until ninst).foreach(i => saved(i).inst := data(sel)(i))
-            (0 until ninst).foreach(i => saved(i).valid := inst_valid(i))
+            (1 until instperbank).foreach(i => saved(i).inst := data(sel)(i))
+            (0 until instperbank).foreach(i => saved(i).valid := inst_valid(i))
           }
         }
       }
@@ -210,11 +213,11 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
         // * burst transport * //
         when(!io.axi.r.bits.last) {
           axi_cnt.inc()
-          data_wstrb(lru(vset))(0) := data_wstrb(lru(vset))(ninst - 1)
-          (1 until ninst).foreach(i => data_wstrb(lru(vset))(i) := data_wstrb(lru(vset))(i - 1))
+          data_wstrb(lru(vset))(0) := data_wstrb(lru(vset))(instperbank - 1)
+          (1 until instperbank).foreach(i => data_wstrb(lru(vset))(i) := data_wstrb(lru(vset))(i - 1))
         }.otherwise {
           rready                := false.B
-          data_wstrb(lru(vset)) := 0.U.asTypeOf(Vec(ninst, UInt(4.W)))
+          data_wstrb(lru(vset)) := 0.U.asTypeOf(Vec(instperbank, UInt(4.W)))
           tag_wstrb(lru(vset))  := false.B
         }
       }.elsewhen(!io.axi.r.ready) {
@@ -224,7 +227,7 @@ class ICache(cacheConfig: CacheConfig)(implicit config: CpuConfig) extends Modul
     is(s_save) {
       when(!io.cpu.cpu_stall && !io.cpu.icache_stall) {
         state := s_idle
-        (0 until ninst).foreach(i => saved(i).valid := false.B)
+        (0 until instperbank).foreach(i => saved(i).valid := false.B)
       }
     }
   }
